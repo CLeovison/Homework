9a21814b634af7a4aee126418dd669970237f350
Leovison <cleovison@gmail.com>
2024-01-17 18:07:47 +0800

    Modified Color Picker

--- color-picker/src/pages/ColorDisplay.jsx
+++ color-picker/src/pages/ColorDisplay.jsx
@@ -1,21 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ColorPicker from "./ColorPicker";
 
 export default function ColorDisplay() {
-  const [mainColor, setMainColor] = useState(() =>{
-      try{
-        const savedColor = localStorage.getItem('colors');
-        return savedColor ? JSON.parse(savedColor) : [];
-      }catch(error){
-        console.log("Error reading from local storage", error);
-        return [];
-      }
-  })
-
+ 
 
   return (
     <>
+
+  
       <ColorPicker />
+     
     </>
   );
 }
--- color-picker/src/pages/ColorPicker.jsx
+++ color-picker/src/pages/ColorPicker.jsx
@@ -2,31 +2,32 @@ import React, { useState, useEffect } from "react";
 import styles from "../styles/ColorPicker.module.css";
 export default function ColorPicker({ selected }) {
   const [color, setColor] = useState("#000000");
-
+  const [value, setValue] = useState(' ')
   //In the Function i need to input the variable that need to be set inorder to set the Value that has been needed
   function handleColor(event) {
     //Inorder to be pass the value that the setColor needs -
     //- i need to put a parameter and the target value inorder to execute the data that has been needed
     setColor(event.target.value);
   }
-
+  function handleSubmit(){
+    
+  }
   return (
     <>
       <div className={styles.maincontent}>
         <h1 className={styles.header}>Color Picker</h1>
 
-        <p style={{color:color}}>Selected Color: {color}</p>
+        <div className={styles.boxtext} style={{ backgroundColor: color }}></div>
         <form action="">
           <label htmlFor="color">
             Choose A Color:
             <input
-              type="color"
+              type="text"
               id="color"
-              value={color} 
-
               //Whenever i set something i need to have a function and an onchange inorder handle the changes that has been put
               onChange={handleColor}
             />
+            <button onClick={handleColor}>Submit</button>
           </label>
         </form>
       </div>
